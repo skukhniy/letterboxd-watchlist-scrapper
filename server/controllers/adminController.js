06ca8f66e73735d7008cfbdb53ecf954052b5ffe
Stan <skukhniy@gmail.com>
2022-10-29 13:24:38 -0400

    cleaned up controller files

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -6,58 +6,8 @@ const { stringify } = require("csv-stringify");
 const { title } = require("process");
 const Monthly = require("../models/NewStreaming");
 
-function scrapeNewStreaming() {
-	const url =
-		"https://comicbook.com/movies/news/streaming-october-2022-new-movies-tv-netflix-disney-plus-hbo-max-hulu-peacock-paramount/#1";
-	// const url =
-	// 	"https://comicbook.com/movies/news/streaming-new-september-2022-netflix-disney-plus-hbo-max-paramount-peacock/#4";
-	const newToStream = [];
-
-	axios(url)
-		.then((res) => {
-			// load html page into cheerio
-			const html = res.data;
-			const $ = cheerio.load(html);
-			// filter to content divs, loop through each div
-			$(".content-gallery-content div", html).each((i, el) => {
-				const div = $(el).html();
-
-				// filter again by divs that include the <strong> tag
-				if (div.includes("<strong>")) {
-					const date = $(el).find("h2").html(); // save the date for this div
-					const pTags = $(el).find("p").toArray(); // selector of <p> tags in this loop
-
-					// loop through each paragraph element
-					pTags.forEach((elem) => {
-						const selector = $(elem);
-						// save name of streaming service in this paragraph tag
-						const streamService = selector.find("strong").html();
-						// find all movie titles and save as Array
-						const movies = selector.find("em").toArray();
-
-						// loop through array of movies
-						movies.forEach((movie) => {
-							const movieSelector = $(movie);
-							let movieTitle = movieSelector.html();
-							movieTitle = movieTitle.replace("<br>", "");
-							movieTitle = movieTitle.replace("&amp;", "&");
-							// create new movie object, append to main array
-							newObj = {
-								movie: movieTitle,
-								streaming: streamService,
-								date: date,
-							};
-							newToStream.push(newObj);
-						});
-					});
-				}
-			});
-			console.log(newToStream);
-			return newToStream;
-		})
-		.catch((err) => console.log(err));
-}
-
+// scrape list of movies from comicbook.com new streaming article
+// upload movie objs to the DB
 exports.uploadMonthly = async (req, res) => {
 	// delete previous documents in the collection
 	await Monthly.remove({});
