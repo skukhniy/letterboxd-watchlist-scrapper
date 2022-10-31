4d3f559f4d91fe51fba64b47468e704597930417
Stan <skukhniy@gmail.com>
2022-10-30 22:51:25 -0400

    adjusted p tag

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Letterboxd Watchlist Movies Now Streaming</h1>
+			<p>Enter Your Letterboxd Username:</p>
+			<input></input>
+		</div>
+	);
 }
 
 export default App;
