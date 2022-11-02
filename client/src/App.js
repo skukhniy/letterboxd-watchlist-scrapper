ffbc2d7a7998e948c829f13bfee3cb3daf38e5fa
Stan <skukhniy@gmail.com>
2022-11-01 20:44:09 -0400

    added router/axios library and basis for func to fetch watchlist streaming list

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import "./styles/App.scss";
+import { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+	const [boxdStreaming, setBoxdStreaming] = useState([]);
+	const fetchBoxdStreaming = async (user) => {
+		const response = await fetch(`/api/${user}`);
+		const json = await response.json();
+
+		if (response.ok) {
+			setBoxdStreaming(json);
+			console.log(json);
+		}
+	};
+
 	return (
 		<div className="App">
 			<div className="text-center mt-5">
-				<h1>Letterboxd Watchlist Movies Now Streaming</h1>
+				<h1>Watchboxd</h1>
+				<p>
+					Enter your letterboxd username below to see what movies on your
+					watchlist came out on streaming this month!
+				</p>
 			</div>
 			<div className="text-center">
-				<p>Enter Your Letterboxd Username:</p>
-				<input></input>
+				<p>Letterboxd Username:</p>
+				<form action="get">
+					<input></input>
+				</form>
 			</div>
 		</div>
 	);
