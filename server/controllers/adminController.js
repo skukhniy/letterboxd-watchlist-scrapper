458ee699b0edd67ee96eb2e3beb06574e8242212
Stan <skukhniy@gmail.com>
2022-12-04 19:00:33 -0500

    added some fixes for the new article format. Function will now look for the streaming title by matching the string with the list of streaming service strings. if theres a match, the loop commences. added few adjustments in the loop to accomdate for comicbook.com's removal of strong and removal of em tags

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -15,12 +15,20 @@ exports.uploadMonthly = async (req, res) => {
   const url =
     'https://comicbook.com/movies/news/streaming-new-movies-tv-december-2022-netflix-disney-plus-hbo-max/';
   // const url =
-  // 	"https://comicbook.com/movies/news/november-new-streaming-movies-tv-netflix-disney-plus-hbo-max/";
+  //   'https://comicbook.com/movies/news/november-new-streaming-movies-tv-netflix-disney-plus-hbo-max/';
   // const url =
   // 	"https://comicbook.com/movies/news/streaming-october-2022-new-movies-tv-netflix-disney-plus-hbo-max-hulu-peacock-paramount/#1";
   // const url =
   // 	"https://comicbook.com/movies/news/streaming-new-september-2022-netflix-disney-plus-hbo-max-paramount-peacock/#4";
-
+  const streamingStrings = [
+    'NETFLIX',
+    'HULU',
+    'HBO MAX',
+    'PARAMOUNT+',
+    'PEACOCK',
+    'PRIME VIDEO',
+    'DISNEY+',
+  ];
   await axios(url)
     .then((res) => {
       // load html page into cheerio
@@ -29,33 +37,54 @@ exports.uploadMonthly = async (req, res) => {
       // filter to content divs, loop through each div
       $('.content-gallery-content div', html).each((i, el) => {
         const div = $(el).html();
-
+        // console.log(`div = ${div}`);
         // filter again by divs that include the <strong> tag
-        if (div.includes('<strong>')) {
+        // if (div.includes('<strong>')) {
+        if (streamingStrings.some((s) => div.includes(s))) {
+          // console.log(`Streaming div located!`);
           const date = $(el).find('h2').html(); // save the date for this div
           const pTags = $(el).find('p').toArray(); // selector of <p> tags in this loop
 
           // loop through each paragraph element
           pTags.forEach((elem) => {
             const selector = $(elem);
+            // console.log(`pTag elem = ${selector}`);
             // save name of streaming service in this paragraph tag
-            const streamService = selector.find('strong').html();
+            let streamingService = 'null';
+            // loop through streaming Strings to find which one is in this particular div
+
+            streamingStrings.forEach((service) => {
+              if (selector.find(service)) {
+                streamingService = service;
+              }
+            });
+            // const streamService = selector.find('strong').html();
+
             // find all movie titles and save as Array
-            const movies = selector.find('em').toArray();
+
+            // const movies = selector.find('em').toArray();
+
+            const selectorString = selector.html();
+            const movies = selectorString.split('<br>');
+
+            // console.log('selector');
+            // console.log(selector.html());
+            console.log(movies);
 
             // loop through array of movies
             movies.forEach((movie) => {
               const movieSelector = $(movie);
               let movieTitle = movieSelector.html();
+              console.log(movieTitle);
               movieTitle = movieTitle.replace('<br>', '');
               movieTitle = movieTitle.replace('&amp;', '&');
               // create new movie object, append to main array
               const newObj = new Monthly({
                 movieTitle: movieTitle,
-                streamingService: streamService,
+                streamingService: streamingService,
                 date: date,
               });
-              console.log(newObj);
+              // console.log(newObj);
               newObj.save();
             });
           });
